b32ce7db3666595aa1a243632578710e102d8f22
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-04 11:31:19 -0600

    000 - Primer commit, configuración básica y .gitignore

---
+++ src/org/javiersian/system/Principal.java
@@ -0,0 +1,14 @@
+
+package org.javiersian.system;
+
+/**
+ *
+ * @author Javier Sian
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+    
+}
