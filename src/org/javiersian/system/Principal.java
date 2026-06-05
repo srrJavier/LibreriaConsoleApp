2fd7ed3d25280e4b845559ea127ca3adcf0b5787
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-05 10:45:10 -0600

    010 se actualizo la clase Principal, agregue un saludo

--- src/org/javiersian/system/Principal.java
+++ src/org/javiersian/system/Principal.java
@@ -8,7 +8,8 @@ package org.javiersian.system;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo soy Javier");
+        System.out.println("Estoy en Kinal - 2026057");
     }
     
 }
